8416d056b8e9376fd7bc9c26a6b536472aa6ef6f
Kojied <kojiedgarmiyazaki@gmail.com>
2016-07-25 15:05:28 +0900

    Initial commit

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,39 @@
+class IdeasController < ApplicationController
+	def index
+		@ideas=Idea.all.paginate(:page => params[:page], :per_page => 10)
+	end
+
+	def create
+		@idea = Idea.create(idea_params)
+		redirect_to root_path
+	end
+
+	def edit
+		@idea = Idea.find(params[:id])
+	end
+
+	def update
+	  @idea = Idea.find(params[:id])
+	  if @idea.update(idea_params)
+	    redirect_to root_path
+	  else
+	    redirect_to edit_idea_path(params[:id])
+	  end
+	end
+
+	def destroy
+		@idea = Idea.find(params[:id])
+		@idea.destroy
+	  redirect_to :back
+	end
+
+	def random
+		@idea = Idea.order("RANDOM()").first
+	end
+
+	private
+
+	def idea_params
+		params.require(:idea).permit(:description,:author)
+	end
+end
